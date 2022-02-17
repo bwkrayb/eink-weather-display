30fd8afd2021d8ff3e75a214cbe486662ef67bf6
Brandon Kraybill <bwkrayb@gmail.com>
2022-02-16 21:58:41 -0600

    fixed if statemnts for night icons and rain

--- libs/functions.py
+++ libs/functions.py
@@ -22,8 +22,7 @@ def get_icon(weatherID):
     dt = datetime.now()
     thunder = (200,201,202,210,211,212,221,230,231,232)
     drizzle = (300,301,301,310,311,312,313,314)
-    lightRain = (500,520)
-    heavyRain = (501,502,503,504,511,521,522,531)
+    rain = (500,501,502,503,504,511,520,521,522,531)
     snow = (600,601,602,611,612,613,615,616,620,621,622)
     fogMist = (701,721,741)
     ptCloud = (801,802,803)
@@ -36,7 +35,7 @@ def get_icon(weatherID):
             return Image.open("images/jpg/day_partial_cloud.jpg")
         elif weatherID in cloudy:
             return Image.open("images/jpg/cloudy.jpg")
-        elif weatherID in lightRain:
+        elif weatherID in rain:
             return Image.open("images/jpg/rain.jpg")
         elif weatherID in fogMist:
             return Image.open("images/jpg/mist.jpg")
@@ -48,11 +47,11 @@ def get_icon(weatherID):
     else:
         if weatherID in clear:
             return Image.open("images/jpg/night_clear.jpg")
-        if weatherID in ptCloud or weatherID in cloudy:            
+        elif weatherID in ptCloud or weatherID in cloudy:            
             return Image.open("images/jpg/night_partial_cloud.jpg")
-        if weatherID in lightRain:
+        elif weatherID in rain:
             return Image.open("images/jpg/night_rain.jpg")
-        if weatherID in snow:
+        elif weatherID in snow:
             return Image.open("images/jpg/night_snow.jpg")
         else:
             logging.info(dt.strftime('%x-%X') + ":No icon set for " + str(weatherID))
